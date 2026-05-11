50940d5fd4663b6158ff6e2cdabfaa1f15873e07
sejiro06 <145810808+sejiro06@users.noreply.github.com>
2026-05-11 09:32:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -145,4 +145,4 @@ def sales():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False
+    app.run(host='0.0.0.0', port=port, debug=False)
