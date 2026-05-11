af33e223900b556da04bf2dfcf519a9f860c7d37
sejiro06 <145810808+sejiro06@users.noreply.github.com>
2026-05-11 09:14:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from datetime import datetime, date
 app = Flask(__name__)
 app.secret_key = "princess_inventory_secret_key_2026"
 
-BASE_DIR = os.path.abspath(os.path.dirname(__    file__))
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 DB_PATH = os.path.join(BASE_DIR, 'princess_inventory.db')
 
 def init_db():
